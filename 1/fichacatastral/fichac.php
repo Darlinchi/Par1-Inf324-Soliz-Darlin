a2bf6955bc24cc7f1c6907d3d7bc157f9388c908
Darlinchi <darlinnatalysolizquispe@gmail.com>
2024-10-04 11:50:51 -0400

    Add files via upload

---
+++ 1/fichacatastral/fichac.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Catastro HAM-LP</title>
+
+  <!-- Fonts Google -->
+  <link href="https://fonts.googleapis.com/css?family=Dosis:400,500|Poppins:400,700&display=swap" rel="stylesheet">
+  
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="../css/bootstrap.css">
+
+  <!-- Custom Styles -->
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/style.css.map">
+  <link rel="stylesheet" href="../css/style.scss">
+
+  <!-- Responsive Styles -->
+  <link rel="stylesheet" href="../css/responsive.css">
+</head>
+<body>
+  <div class="hero_area">
+    <!-- Header Section -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+          <a class="navbar-brand" href="index.php">
+            <img src="../images/logocasa.png" alt="Logo La Paz" style="width: 60px; height: auto;">
+            <span>Ficha Catastral</span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="../index.php">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../normativa/normativa.php">Normativa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="fichac.php">Ficha Catastral</a>
+              </li>
+            </ul>
+            <div class="quote_btn-container">
+              <a href="tel:+011234567890">Llama: +591 76543210</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+
+  <!-- service section -->
+  <section class="service_section layout_padding">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 offset-md-2">
+          <h2 class="custom_heading">
+          Ficha <span>Catastral</span>
+          </h2>
+          <br>
+          <div class="detail-box">
+          <h3 class="custom_heading">
+          La Autoridad Catastral Municipal pone a disposición del público en general las nuevas Fichas Catastrales, Los que se utilizan para realizar el Registro Catastral en el Municipio de La Paz.
+          </h3> <br>
+          <h2 class="custom_heading">
+          <span>Guía de llenado de la Ficha Catastral</span>
+          </h2>
+          <div style="padding: 20px;">
+          <div style="margin-bottom: 10px;">
+            <a href="https://sitservicios.lapaz.bo/sit/catastro/fichacatastral/guia/" target="_blank">
+              &#x1F4D1; Guía de llenado de la Ficha Catastral 
+            </a>
+          </div>
+
+          <div style="margin-bottom: 10px;">
+            <a href="https://sitservicios.lapaz.bo/sit/catastro/fichacatastral/docs/guia-ficha-catastral-A-B-C.pdf" target="_blank">
+              &#x1F4D1; Guía de llenado de la Ficha Catastral (*.PDF)
+            </a>
+          </div>
+
+          <div style="margin-bottom: 10px;">
+            <a href="https://sitservicios.lapaz.bo/sit/catastro/fichacatastral/docs/GUIA%20DE%20LLENADO%20DE%20LA%20FICHA%20CATASTRAL%20A,B,C.PDF" target="_blank">
+              &#x1F4D1; Guía de llenado de la Ficha Catastral (PDF resumido)
+            </a>
+          </div>
+
+          <h2 class="custom_heading">
+          <span>Descargar Ficha Catastral de la Autoridad Catastral Municipal en formato PDF.</span>
+          </h2>
+          
+          <div style="margin-bottom: 10px;">
+            <a href="https://sitservicios.lapaz.bo/sit/catastro/fichacatastral/docs/FICHA%20CATASTRAL%20A%20(SMP%20ACM).pdf" target="_blank">
+              &#x1F4D1; Ficha Catastral A Propiedad Unifamiliar - ACM (*.pdf)
+            </a>
+          </div>
+
+          <div style="margin-bottom: 10px;">
+            <a href="https://sitservicios.lapaz.bo/sit/catastro/fichacatastral/docs/FICHA%20CATASTRAL%20B%20(SMP%20ACM).pdf" target="_blank">
+              &#x1F4D1; Ficha Catastral B Matriz PH - ACM (*.pdf)
+            </a>
+          </div>
+
+          <div style="margin-bottom: 10px;">
+            <a href="https://sitservicios.lapaz.bo/sit/catastro/fichacatastral/docs/FICHA%20CATASTRAL%20C%20(SMP%20ACM).pdf" target="_blank">
+              &#x1F4D1; Ficha Catastral C Unidad PH - ACM (*.pdf)
+            </a>
+          </div>
+
+          <h2 class="custom_heading">
+          <span>FICHA CATASTRAL DIGITAL (FDRC version 2)</span>
+          </h2>
+
+          <h3 class="custom_heading">
+          Se ha actualizado el Formulario Digital de Registro Catastral (FDRC) a la nueva Ficha Catastral y esta disponible para AutoCAD y Windows.
+          </h3>
+
+          <div style="margin-bottom: 10px;">
+            <a href="http://sitservicios.lapaz.bo/sit/FDRC" target="_blank">
+              &#x1F4D1; Ficha Catastral Digital (FDRC)
+            </a>
+          </div>
+
+          <div style="margin-bottom: 10px;">
+            <a href="http://sitservicios.lapaz.bo/sit/FDRC/descargas.html" target="_blank">
+              &#x1F4D1; Descargar aplicativo Ficha Catastral
+
+            </a>
+          </div>
+        </div>
+          </div>
+        </div>
+            <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 offset-md-2">
+                <h2 class="custom_heading">
+                PLATAFORMAS DE ATENCIÓN CATASTRO Y <span>ADMINISTRACIÓN TERRITORIAL</span>
+                </h2>
+                <div class="container layout_padding2">
+                    <div class="row">
+                    <div class="col-md-4">
+                        <div class="img_box">
+                        <img src="../images/location-white.png" alt="">
+                        </div>
+                        <div class="detail_box">
+                        <h6>
+                            PLATAFORMA CATASTRO
+                        </h6>
+                        <p>      
+                            Calle Potosí N° 1285, Edificio Tobía Planta Baja<br>
+                            ZONA CENTRAL<br>
+                            Trámites Catastro IP (265)2082
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="img_box">
+                        <img src="../images/location-white.png" alt="">
+                        </div>
+                        <div class="detail_box">
+                        <h6>
+                        SERMAT I-II
+                        </h6>
+                        <p>
+                        Planta Baja. Edificio SOBOCE. Calle Socabaya casi esq. Mercado.<br>
+                        ZONA CENTRAL<br>
+                        Trámites Territorio IP (265)2085
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="img_box">
+                        <img src="../images/location-white.png" alt="">
+                        </div>
+                        <div class="detail_box">
+                        <h6>
+                        PIAC SUR-SERMAT III
+                        </h6>
+                        <p>
+                        Av. José Ballivián Nº 720 Entre Calles 13 y 14, Edificio Atlanta<br>
+                        CALACOTO<br>
+                        Trámites Catastro y territorio IP (265)2088
+                        </p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- info section -->
+  <section class="info_section layout_padding2">
+    <div class="container">
+      <div class="info_items">
+        <a href="https://sitservicios.lapaz.bo/mapas/plataformas/" target="_blank">
+          <div class="item ">
+            <div class="img-box box-1">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+              PLATAFORMAS DE ATENCIÓN
+              </p>
+            </div>
+          </div>
+        </a>
+        <a href="https://igob247.lapaz.bo" target="_blank">
+          <div class="item ">
+            <div class="img-box box-2">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+              iGob24/7
+              </p>
+            </div>
+          </div>
+        </a>
+        <a href="https://www.lapaz.bo" target="_blank">
+          <div class="item ">
+            <div class="img-box box-3">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+              LaPaz.bo
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info_section -->
+
+<!-- Footer Section -->
+<footer class="footer_section">
+  <div class="container">
+        <p>&copy; 2024 Todos los derechos reservados por Mapa Catastral La Paz - Bolivia</a></p>
+  </div>
+</footer>
+<script src="../js/bootstrap.js"></script>
+<script src="../js/jquery-3.4.1.min.js"></script>
+</body>
+</html>
---
+++ 1/footer.php
@@ -0,0 +1,36 @@
+  <!-- footer section -->
+  <section class="container-fluid footer_section">
+  <p>&copy; 2024 Todos los derechos reservados por Mapa Catastral La Paz - Bolivia</a></p>
+  </section>
+  <!-- footer section -->
+
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+  <script>
+    function initMap() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: {
+          lat: -16.500000,
+          lng: -68.150002
+        },
+      });
+
+      var image = 'images/maps-and-flags.png';
+      var beachMarker = new google.maps.Marker({
+        position: {
+          lat: -16.500000,
+          lng: -68.150002
+        },
+        map: map,
+        icon: image
+      });
+    }
+  </script>
+  <!-- google map js -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
+  <!-- end google map js -->
+</body>
+
+</html>
